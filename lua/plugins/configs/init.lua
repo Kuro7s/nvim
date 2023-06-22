eb073452b469a097c7d8bbab4042f4fde57b0893
Kuro7s <kuro0970gamer@gmail.com>
2023-06-22 19:13:41 -0400

    Some progress

---
+++ lua/plugins/configs/init.lua
@@ -0,0 +1,6 @@
+require("plugins.configs.nvim-treesitter")
+require("plugins.configs.gitsigns")
+require("plugins.configs.lualine")
+require("plugins.configs.nvim-tree")
+require("plugins.configs.telescope")
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,14 +1,13 @@
-require("plugins.configs.nvim-treesitter")
-require("plugins.configs.gitsigns")
+require("plugins.configs")
 
 -- Setup package manager
 local ensure_packer = function()
     local fn = vim.fn
     local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
     if fn.empty(fn.glob(install_path)) > 0 then
-      fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-      vim.cmd [[packadd packer.nvim]]
-      return true
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
     end
     return false
 end
@@ -17,26 +16,33 @@ local packer_bootstrap = ensure_packer()
 
 -- Initializing package manager and package listing
 return require('packer').startup(function(use)
-    -- Packer itself
     use 'wbthomason/packer.nvim'
 
     -- Themes
     use 'AhmedAbdulrahman/aylin.vim'
     use 'catppuccin/nvim'
-
     -- Treesitter
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-
-    -- Icons
+    -- Icons (used by barbar, nvim-tree, and lualine)
     use 'nvim-tree/nvim-web-devicons'
-
+    -- Git
     use 'lewis6991/gitsigns.nvim'
+    -- Tabline
     use 'romgrk/barbar.nvim'
+    -- Statusline
+    use 'nvim-lualine/lualine.nvim'
+    -- File explorer
+    use 'nvim-tree/nvim-tree.lua'
+    -- Fuzzy finder
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
-      require('packer').sync()
+        require('packer').sync()
     end
 end)
 
